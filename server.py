230dcdbdae0ffc4308f19c6ac09ad1b8c2f52823
Pranav <pranavbairy2@gmail.com>
2025-06-19 22:44:42 +0530

    Testing video

--- server.py
+++ server.py
@@ -2,13 +2,16 @@ from flask import Flask, request, jsonify, render_template, send_from_directory
 from generate_media import list_videos, convert_to_mp4
 import json
 
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     movie_list = json.load(open("./videos.json", "r")) 
     return render_template('index.html', movies=movie_list)
+
+@app.route('/media/<path:filename>')
+def media(filename):
+    return send_from_directory('media', filename)
 
 @app.route("/Watching")
 def movies():
