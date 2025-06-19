4e7a52ecf000d9b7c1cf8dadf2756487ca9af837
bp2881 <pranavbairy2@gmail.com>
2025-06-19 21:46:40 +0530

    Creating index

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     movie_list = json.load(open("./videos.json", "r")) 
-    return render_template('index.html', movie_list)
+    return render_template('index.html', movies=movie_list)
 
 @app.route("/Watching")
 def movies():
@@ -18,4 +18,4 @@ def movies():
 if __name__ == "__main__":
     #convert_to_mp4()  
     list_videos()
-    app.run(host='127.0.0.1', debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=5000)
